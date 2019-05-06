3c1da66d337a0db505c1fc13107b2d6251891f13
Ethan Brann <ethan.brann94@gmail.com>
2019-05-06 22:46:32 +0100

    tweaked styling, added more projects

--- scripts/scroll-nav.js
+++ scripts/scroll-nav.js
@@ -1,20 +1,17 @@
 (function scrollNav() {
-    const mainElement = document.querySelector('main');
     const about = document.querySelector('#about');
     const projects = document.querySelector('#projects');
-    // const skills = document.querySelector('#skills');
-    const home = document.querySelector('.logo');
 
     const targets = {
-        about: about.getBoundingClientRect().top,
-        projects: projects.getBoundingClientRect().top,
+        about: about.getBoundingClientRect().top + window.scrollY,
+        projects: projects.getBoundingClientRect().top + window.scrollY,
         home: 0
     }
 
     const links = {
         about: document.getElementById('about-link'),
         projects: document.getElementById('projects-link'),
-        home
+        home: document.querySelector('.logo')
     }
 
     window.addEventListener('resize', () => {
@@ -26,17 +23,13 @@
         const scrollY = window.scrollY;
         const amtToMove = Math.abs(target - scrollY);
         let time = 0;
-        console.log(amtToMove);
 
         function easeInOutQuad (t) { 
             return t<.5 ? 2*t*t : -1+(4-2*t)*t 
         }
         
         const scroll = () => {
-            // console.log('scroll called');
-            // console.log(window.scrollY);
             if (scrollY < target) {
-                console.log(scrollY + easeInOutQuad(time) * amtToMove);
                 window.scroll(0, scrollY + easeInOutQuad(time) * amtToMove);
             } else if (scrollY > target) {
                 window.scroll(0, scrollY - easeInOutQuad(time) * amtToMove);
